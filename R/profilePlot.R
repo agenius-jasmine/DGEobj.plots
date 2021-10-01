13a110165e84350eb55b3a11c6bc251ff774f59a
Jasmine Lai <jasmine@aggregate-genius.com>
2021-10-01 11:29:38 -0700

    update help documentation for geneNameCol to specify that is only works for CCX plots

--- R/profilePlot.R
+++ R/profilePlot.R
@@ -35,8 +35,8 @@
 #' @param ylab Y axis label (defaults = "LogRatio column name")
 #' @param title Plot title (optional)
 #' @param pthreshold the p value threshold used to color points (default = 0.01)
-#' @param geneNameCol geneName column in geneData from DGEobj. This column will be used to label
-#'    significantly changed points.
+#' @param geneNameCol geneName column in geneData from DGEobj. This column will be used to add the gene name
+#'        in the popover for canvasXpress plots only.
 #' @param sizeBySignificance Set to TRUE to size points by the negative Log10 of the
 #'        Significance measure (default = FALSE)
 #' @param referenceLine Color for a horizontal reference line at intercept = 0
